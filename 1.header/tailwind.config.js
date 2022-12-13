33fe50ea7ad70a4ab3e3c39cd9099e28196d211c
BomsTechnology <marcsigha@gmail.com>
2022-12-13 15:32:12 +0100

    start header/footer/newsletter

---
+++ 1.header/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "header.html"],
+  prefix: "boms12_171222-header-",
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: "Roboto",
+      },
+    },
+  },
+  plugins: [],
+};
