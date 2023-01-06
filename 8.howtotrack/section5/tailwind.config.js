467bf4a54b2396cae0890fdfc5694625e6e47601
BomsTechnology <marcsigha@gmail.com>
2023-01-06 18:52:10 +0100

    add new section

---
+++ 8.howtotrack/section5/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "index.html"],
+  prefix: "boms12_171222-htw_5-",
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat",
+      },
+    },
+  },
+  plugins: [],
+};
